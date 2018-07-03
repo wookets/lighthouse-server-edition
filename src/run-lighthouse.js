c167b3eecd014385b5c81ea45174bc9b221e8d24
Sean Wesenberg <wookets@gmail.com>
2018-07-02 21:48:30 -0500

    Added a simple express route

---
+++ src/run-lighthouse.js
@@ -0,0 +1,26 @@
+
+const lighthouse = require('lighthouse')
+const chromeLauncher = require('chrome-launcher')
+
+function launchChromeAndRunLighthouse(url, opts, config = null) {
+  return chromeLauncher.launch({chromeFlags: opts.chromeFlags}).then(chrome => {
+    opts.port = chrome.port;
+    return lighthouse(url, opts, config).then(results => {
+      // use results.lhr for the JS-consumeable output
+      // https://github.com/GoogleChrome/lighthouse/blob/master/typings/lhr.d.ts
+      // use results.report for the HTML/JSON/CSV output as a string
+      // use results.artifacts for the trace/screenshots/other specific case you need (rarer)
+      return chrome.kill().then(() => results.lhr)
+    })
+  })
+}
+
+const opts = {
+  chromeFlags: ['--show-paint-rects']
+}
+
+// Usage:
+launchChromeAndRunLighthouse('https://www.google.com', opts).then(results => {
+  // Use results!
+  console.log('results', results)
+})
